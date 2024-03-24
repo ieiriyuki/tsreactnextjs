ca2a110e7a5b32d475a73901613c1a25bbc5fa81
ieiriyuki <ieiriyuki@example.com>
2024-03-24 16:41:29 +0900

    feat: sample type guard

---
+++ chapter2/typeguard.ts
@@ -0,0 +1,13 @@
+type USer = {
+    info?: {
+        name: string
+        age: number
+    }
+}
+
+let response = {}
+const user = (response as any) as USer
+
+if (user.info) {  // type is inferred
+    console.log(user.info.name)
+}
