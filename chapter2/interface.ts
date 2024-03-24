ee58de4a8b525683f1b48561fe09e7bc5469fec1
ieiriyuki <ieiriyuki@example.com>
2024-03-24 13:57:09 +0900

    feat: sample interface

---
+++ chapter2/interface.ts
@@ -0,0 +1,17 @@
+interface Point {
+    x: number;
+    y: number;
+}
+
+function printPoint(point: Point) {
+    console.log(`x: ${point.x}, y: ${point.y}`)
+}
+
+interface Point {
+    z: number;
+}
+
+// compile error
+// printPoint({ x: 1, y: 2 })
+
+printPoint({ x: 1, y: 2, z: 3 })
