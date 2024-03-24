0e995dc88cc1a87905bbec34d3ea447cc6c3ec21
ieiriyuki <ieiriyuki@example.com>
2024-03-24 14:22:40 +0900

    feat: sample class extends

--- chapter2/classes.ts
+++ chapter2/classes.ts
@@ -19,3 +19,20 @@ class Point {
 const point = new Point()
 point.moveX(1)
 console.log(`${point.x}, ${point.y}`)
+
+class Point3D extends Point {
+    z: number
+
+    constructor(x: number = 0, y: number = 0, z: number = 0) {
+        super(x, y)
+        this.z = z
+    }
+
+    moveZ(z: number) {
+        this.z += z
+    }
+}
+
+const point3D = new Point3D()
+point3D.moveZ(1)
+console.log(`${point3D.x}, ${point3D.y}, ${point3D.z}`)
