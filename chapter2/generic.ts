b1639a20ed1782a532c6bb9fe461f6ce5d6b22e2
ieiriyuki <ieiriyuki@example.com>
2024-03-24 16:20:36 +0900

    feat: sample generic code

---
+++ chapter2/generic.ts
@@ -0,0 +1,16 @@
+class Queue<T> {
+    private array: T[] = []
+
+    push(item: T) {
+        this.array.push(item)
+    }
+
+    pop(): T | undefined {
+        return this.array.shift()
+    }
+}
+
+const queue = new Queue<number>()
+queue.push(1)
+queue.push(2)
+// queue.push('foo') // compile error
