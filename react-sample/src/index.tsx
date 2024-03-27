c0a278fb94e5ce111d7ccb737bb7ad2383871869
ieiriyuki <ieiriyuki@example.com>
2024-03-27 23:25:25 +0900

    feat: sample

--- react-sample/src/index.tsx
+++ react-sample/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Hello from './components/Hello';
+import Name from './components/Name';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Hello />
+    <Name />
   </React.StrictMode>
 );
 
