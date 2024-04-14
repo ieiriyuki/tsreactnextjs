6c8d258a777ea7a943aa76ae7016604ab6f6a628
ieiriyuki <ieiriyuki@example.com>
2024-04-14 14:39:10 +0900

    feat: new theme

---
+++ next-sample/theme.ts
@@ -0,0 +1,12 @@
+export const theme = {
+  space: ['0px', '4px', '8px', '16px', '24px', '32px'],
+  colors: {
+    white: '#fff',
+    black: '#000',
+    red: '#f00',
+  },
+  fontSizes: ['12px', '14px', '16px', '18px', '20px', '23px'],
+  fonts: {
+    primary: `arial, sans-serif`,
+  },
+}
