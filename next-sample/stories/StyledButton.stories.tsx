0efb10dc7af0e5d16ff7390f2f03371182372d26
ieiriyuki <ieiriyuki@example.com>
2024-04-14 16:59:04 +0900

    refactor: action on storybook

--- next-sample/stories/StyledButton.stories.tsx
+++ next-sample/stories/StyledButton.stories.tsx
@@ -5,6 +5,7 @@ import { StyledButton, StyledButtonProps } from '../components/StyledButton'
 export default {
   title: 'StyledButton',
   component: StyledButton,
+  argTypes: { onClick: { action: 'clicked' } },
 } as ComponentMeta<typeof StyledButton>
 
 export const Primay: Story<StyledButtonProps> = (props) => {
