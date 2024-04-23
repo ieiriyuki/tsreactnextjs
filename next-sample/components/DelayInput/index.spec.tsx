8dfd139efdefcd5ae777e3f5f9c17eab3c0a815b
ieiriyuki <ieiriyuki@example.com>
2024-04-23 20:08:26 +0900

    test: on text input after 1 sec

--- next-sample/components/DelayInput/index.spec.tsx
+++ next-sample/components/DelayInput/index.spec.tsx
@@ -1,4 +1,4 @@
-import { render, screen, RenderResult, fireEvent } from '@testing-library/react'
+import { render, screen, RenderResult, fireEvent, act } from '@testing-library/react'
 import { DelayInput } from './index'
 
 describe('DelayInput', () => {
@@ -6,12 +6,15 @@ describe('DelayInput', () => {
   let handleChange: jest.Mock
 
   beforeEach(() => {
+    jest.useFakeTimers()
     handleChange = jest.fn()
     renderResult = render(<DelayInput onChange={handleChange} />)
   })
 
   afterEach(() => {
     renderResult.unmount()
+    jest.runOnlyPendingTimers()
+    jest.useRealTimers()
   })
 
   it('should display empty in span on initial render', () => {
@@ -25,5 +28,16 @@ describe('DelayInput', () => {
     fireEvent.change(inputNode, { target: { value: inputText } })
     const spanNode = screen.getByTestId('display-text') as HTMLSpanElement
     expect(spanNode.innerHTML).toEqual('入力中')
+  })
+
+  it('should display input text 1 second after onChange event occurs', async () => {
+    const inputText = 'Test Input Text'
+    const inputNode = screen.getByTestId('input-text') as HTMLInputElement
+    fireEvent.change(inputNode, { target: { value: inputText } })
+    await act(() => {
+      jest.runAllTimers()
+    })
+    const spanNode = screen.getByTestId('display-text') as HTMLSpanElement
+    expect(spanNode.innerHTML).toEqual(`入力したテキスト: ${inputText}`)
   })
 })
