4dd1858e90944a9be661886fc450b32a0996704f
ieiriyuki <ieiriyuki@example.com>
2024-04-25 23:50:03 +0900

    feat: basic next code of chapter 5

---
+++ chapter5/nextjs-gihyo-book/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: (() => {
+    let compilerConfig = {
+      // styledComponentsの有効化
+      styledComponents: true,
+    }
+  })
+}
+
+module.exports = nextConfig
